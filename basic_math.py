954e0837a5f7f8e2b2657c34f21650be6aa0b352
Viviana Hurtado <viviana.hurtado-antiporta@stu.ocsb.ca>
2025-02-18 06:44:29 +0000

    Added basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,14 @@
+# Created by: Viviana Hurtado
+# Date: feb 16 2025
+# This programs does basic math
+def main():
+    print("2+3={}".format(2 + 3))
+    print("7-2={}".format(7 - 2))
+    print("5*2={}".format(5 * 2))
+    print("13/4={}".format(13 / 4))
+    print("3**3={}".format(3**3))
+    print("16**0.5={}".format(16**0.5))
+
+
+if __name__ == "__main__":
+    main()
